25c62ab29db4cc12d0a65089c39f8ceba2a25048
kilonzijnr <vkilonzi05@gmail.com>
2021-11-01 13:32:15 +0300

    made depencies installations

--- projects/settings.py
+++ projects/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django-bootstrap5'
+    'cloudinary'
+    'awards'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
